a3787965c8e34fd258199a9959c40686e4130d66
jyuau-mc <240323028@stu.vtc.edu.hk>
2026-04-03 13:01:08 +0000

    update aws

--- learn-terraform-multicloud-kubernetes-main/aws/variables.tf
+++ learn-terraform-multicloud-kubernetes-main/aws/variables.tf
@@ -1,7 +1,29 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-variable "region" {
-  default     = "us-east-2"
+variable "aws_region" {
   description = "AWS region"
+  type        = string
+  default     = "us-east-1"   # Learner Lab 支持的区域
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+  default     = "my-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
 }
